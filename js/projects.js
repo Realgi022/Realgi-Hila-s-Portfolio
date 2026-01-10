5bead4ede0280ca3c2812ffd81cdb31d7559e2a7
Realgi022 <realgihila@gmail.com>
2026-01-10 17:52:19 +0100

    Initial commit - RealgiHilaPortfolio project

---
+++ js/projects.js
@@ -0,0 +1,23 @@
+const projects = [
+    {
+        title: "FootballBettingWebApp",
+        description: "A web application for football betting, built using a three-layered architecture with C#, .NET, and MSSQL. The app handles user management, match tracking, and betting operations efficiently, and runs as a fully functional web application.",
+        technologies: "C#, .NET, MSSQL, HTML, CSS, JavaScript",
+        github: "https://github.com/Realgi022/FootballBettingWebApp.git",
+        demo: "https://i555208.luna.fhict.nl/",
+        screenshots: [
+            "Images/AdminSide.png",
+            "Images/AdminSideCreateMatch.png",
+            "Images/UserSidePlaceBet.png",
+            "Images/UserSidePlaceBetPage.png"
+        ]
+    },
+    {
+        title: "MyPortfolio",
+        description: "A website where I showcase my projects.",
+        technologies: "HTML, CSS, JavaScript",
+        github: "#",
+        demo: "#",
+        screenshots: ["Images/MyPortfolio.png"]
+    }
+];
